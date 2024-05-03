06b9701d6db47e6e99c8f0bf3edb282e63fb75b3
SIST-101 <SIST-101@SIST>
2024-05-03 15:29:24 +0900

    111

--- Chapter05_크롤링_1/src/MusicMain.java
+++ Chapter05_크롤링_1/src/MusicMain.java
@@ -20,11 +20,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import java.io.*;
 import java.net.URLEncoder;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 class Music
 {
-   int rank;
+   String rank;
    String title;
    String singer;
    String album;
@@ -33,7 +34,7 @@ class Music
 }
 class GenieMusic
 {
-	Music[] musics=new Music[200]; // 클래스 블록은 선언하는 위치 
+	static Music[] musics=new Music[200]; // 클래스 블록은 선언하는 위치 
 	/*
 	 *    <td class="info">
                  <a href="#" class="title
@@ -41,7 +42,7 @@ class GenieMusic
 	{
 		try
 		{
-		  int k=1;
+		  /*int k=1;
 		  for(int i=1;i<=4;i++)
 		  {
 			  Document doc=Jsoup.connect("https://www.genie.co.kr/chart/top200?ditc=D&ymd=20240503&hh=14&rtm=Y&pg="+i).get();
@@ -58,10 +59,78 @@ class GenieMusic
 				  System.out.println("이미지:"+poster.get(j).attr("src"));
 				  //System.out.println("동영상 키:"+youtubeData(title.get(j).text()));
 				  System.out.println("===================================================");
+				  FileWriter fw=
+						  new FileWriter("c:\\javaDev\\genie.txt",true);//append
+				  String data=k+"|"
+						     +title.get(j).text()+"|"
+						     +singer.get(j).text()+"|"
+						     +album.get(j).text()+"|"
+						     +poster.get(j).attr("src")+"|"
+						     +youtubeData(title.get(j).text())+"\r\n";
+				  fw.write(data);
+				  fw.close();
+				  k++;
 			  }
+		  }*/
+		  // 파일에서 데이터 읽기
+		  FileReader fr=new FileReader("c:\\javaDev\\genie.txt");
+		  int i=0;
+		  StringBuffer sb=new StringBuffer();
+		  while((i=fr.read())!=-1)
+		  {
+			  sb.append((char)i);
+		  }
+		  fr.close();
+		  
+		  String data=sb.toString();
+		  String[] temp=data.split("\n");
+		  i=0;
+		  for(String s:temp)
+		  {
+			  String[] ss=s.split("\\|");
+			  musics[i]=new Music();
+			  musics[i].rank=ss[0];
+			  musics[i].title=ss[1];
+			  musics[i].singer=ss[2];
+			  musics[i].album=ss[3];
+			  musics[i].poster=ss[4];
+			  musics[i].key=ss[5];
+			  i++;
 		  }
 			
 		}catch(Exception ex){}
+	}
+	static void musicList()
+	{
+		System.out.println("================Music List===============");
+		int i=1;
+		for(Music s:musics)
+		{
+			System.out.println(i+"."+s.title);
+			i++;
+		}
+	}
+	static void musicDetail(int rank)
+	{
+		try
+		{
+			Music m=musics[rank-1];
+			String url="http://youtube.com/embed/"+m.key;
+			Runtime.getRuntime().exec("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe "+url);
+		}catch(Exception ex) {}
+	}
+	static void musicTitleFind(String title)
+	{
+		int count=0;
+		for(Music m:musics)
+		{
+			if(m.title.contains(title))
+			{
+				System.out.println(m.title);
+				count++;
+			}
+		}
+		System.out.println("검색 결과 총 "+count+"건");
 	}
 	static String youtubeData(String title) throws Exception
 	{
@@ -69,12 +138,16 @@ class GenieMusic
 		String url="https://www.youtube.com/results?search_query="
 				  +URLEncoder.encode(title, "UTF-8");
 		Pattern p=Pattern.compile("/watch\\?v=[^가-힣]+");
+		//정규식 
 		Document doc=Jsoup.connect(url).get();
 		Matcher m=p.matcher(doc.toString());
 		while(m.find())
 		{
 			String temp=m.group();
 			System.out.println(temp);
+			/*
+			 *  /watch?v=Ejc__vhP2U8\u0026pp=ygUJ7JiI67uk7Ja0","webPageType":"WEB_PAGE_TYPE_WATCH","rootVe":3832}},"watchEndpoint":{"videoId":"Ejc__vhP2U8","params":"qgMJ7JiI67uk7Ja0ugMLCNWu0ZKv-dGC8gG6Aw8SDVJERWpjX192aFAyVTi6AwoIoPKA1va5u5cFugMKCNjj4YCYlcuDCboDCgjOqJGHgZ2J-Tq6AwsIpdDom4mLw6bIAboDCgiXj7OCiPfQ41y6AwoI4P7S5tLDh4k3ugMJCMfo1sHg_pgIugMLCJbvrufEt8TQ1wG6AwoI-qWf3oPEupNQugMLCJHn2e6Fh5WroAG6AwoIxeCuxdSjkdl2u
+			 */
 			temp=temp.substring(temp.indexOf("=")+1,temp.indexOf("\""));
 			result=temp;
 			break;
@@ -87,11 +160,20 @@ public class MusicMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         GenieMusic gm=new GenieMusic();
-        try
+        gm.musicList();
+        System.out.println("=========================================");
+        Scanner scan=new Scanner(System.in);
+        //System.out.print("1~200사이의 번호 선택:");
+        //int no=scan.nextInt();
+        //gm.musicDetail(no);
+        System.out.print("검색어 입력:");
+        String title=scan.next();
+        gm.musicTitleFind(title);
+        /*try
         {
         	String url="http://youtube.com/embed/ImuWa3SJulY\\u0026pp=ygUM6rOg66-87KSR64-F";
         	Runtime.getRuntime().exec("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe "+url);
-        }catch(Exception ex) {}
+        }catch(Exception ex) {}*/
 	}
 
 }
