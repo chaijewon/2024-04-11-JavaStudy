40e45b2a686a95d634bae2180b3d742dcdcd8c0e
SIST-101 <SIST-101@SIST>
2024-05-08 08:59:55 +0900

    1111

---
+++ Chapter05_ 클래스_1/src/뮤직시스템.java
@@ -0,0 +1,19 @@
+class Music
+{
+	int rank;
+	String title;
+	String poster;
+	String singer;
+	String album;
+	String state;//유지 상승 하강
+	int idcrement;// 등폭 
+	int like;//좋아요 
+}
+public class 뮤직시스템 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
