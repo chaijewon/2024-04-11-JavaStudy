e2344cfcd5464bde50f1d1d67ce126706fab2f02
SIST-101 <SIST-101@SIST>
2024-05-23 15:51:39 +0900

    1111

---
+++ Chapter10_라이브러리(java.util)_2/src/com/sist/util/컬렉션_정리.java
@@ -0,0 +1,31 @@
+package com.sist.util;
+/*
+ *   컬렉션 => 데이터 관리 (배열을 대처) 
+ *                    ---------
+ *                     | 고정 (먼저 메모리 크기를 만들어서 관리 => 추가,삭제가 어렵다)
+ *                                                      -------------
+ *                                                      | 배열을 새롭게 생성 => copyOf => 보완 
+ *    = 종류 
+ *      1) 데이터 중복이 가능여부
+ *         데이터 중복 허용 : List => ArrayList , Map => HashMap 
+ *         데이터 중복 허용(X) : Set
+ *      2) 인덱스이용 여부 
+ *         인덱스 사용 => ArrayList 
+ *         실제값 사용 => HashSet
+ *         key 사용  => HashMap 
+ *         --- 중복을 허용하지 않는다 
+ *      ** 데이터 관리 
+ *         --------- 반드시 필요 => 구분자 
+ *         
+ *          
+ *    = 
+ *    =
+ */
+public class 컬렉션_정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
